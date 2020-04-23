22b96fa6811f98a9b82cfdd4bada75af94b624af
seba258 <olvc.sebastien.stevens@gmail.com>
2020-04-23 15:08:30 +0200

    i want to commit

--- Master program/Wind_rose.py
+++ Master program/Wind_rose.py
@@ -3,14 +3,18 @@ from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 
+dataset = netcdf_dataset('../Data/wind/' + month + '/MERRA2.2005' + month + day + '.A3dyn.05x0625.EU.nc4')
+
 # Create wind speed and direction variables
 
-ws = np.random.random(500) * 6
-wd = np.random.random(500) * 360
+ws = np.random.random(200) * 6
+wd = np.random.random(200) * 360
 
 # Make wind rose plot
 ax = WindroseAxes.from_ax()
-ax.bar(wd, ws, normed=True, opening=0.8, edgecolor='white')
+ax.bar(wd, ws, nsector=8, normed=True, opening=0.8, edgecolor='white')
 ax.set_legend()
 
 plt.show()
+
+print(ws)
