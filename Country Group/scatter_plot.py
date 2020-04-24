66a8836604261e1b30720d5c895724cbc51e6941
KoenPrudhommevanReine <koen.prudhomme@ziggo.nl>
2020-04-24 17:57:30 +0200

    Countries are grouped and normalized, the axis are both logaritmic

--- Country Group/scatter_plot.py
+++ Country Group/scatter_plot.py
@@ -12,17 +12,17 @@ British_Isles = ["British Isles", "Ireland", "United Kingdom"]
 Asian= ["Asian", "Iraq", "Iran", "Palestine","Israel","Lebanon","Jordan","Syria","Saudi Arabia"]
 Iberian_Peninsula = ["Iberian Peninsula", "Spain", "Portugal"]
 Southeast = ["Southeast","Greece","North Macedonia", "Albania", "Bulgaria", "Bosnia and Herzegovina","Serbia","Montenegro"]
-Scandinavia = ["Scandinavia", "Sweden", "Norway", "Denmark"]
+Scandinavia = ["Nordic Countries", "Sweden", "Norway", "Denmark", "Finland"]
 Central_Europe = ["Central Europe", "Hungary", "Croatia", "Slovenia", "Slovakia", "Austria", "Czechia"]
-Turkey = ["Turkey and the Neighbours", "Turkey", "Armenia", "Georgia", "Azerbaijan", "Cyprus"]
+Turkey = ["Turkey etc", "Turkey", "Armenia", "Georgia", "Azerbaijan", "Cyprus"]
 Baltics = ["Baltics", "Estonia", "Latvia", "Lithuania"]
-Eastern_Europe = ["Eastern Europe","Russian Federation","Ukraine","Belarus","Kazakhstan","Moldova"]
+Eastern_Europe = ["Eastern Europe","Russian Federation","Ukraine","Belarus","Kazakhstan","Moldova","Romania"]
 Iceland = ["Icelandics", "Iceland", "Faroes"]
-Romania = ["Romania","Romania"]
-Finland = ["Finland","Finland"]
+# Romania = ["Romania","Romania"]
+# Finland = ["Finland","Finland"]
 Germany = ["Germany","Germany"]
 France = ["France","France"]
-Switzerland = ["Switzerland","Switzerland"]
+Switzerland = ["Switserland","Switzerland"]
 Italy = ["Italy", "Italy"]
 Poland = ["Poland","Poland"]
 Belgium = ["Belgium", "Belgium"]
@@ -33,7 +33,7 @@ Algeria = ["Algeria","Algeria"]
 Libya = ["Libya", "Libya"]
 Tunisia = ["Tunisia", "Tunisia"]
 Egypt = ["Egypt","Egypt"]
-All_groups = [British_Isles, Asian, Iberian_Peninsula, Southeast, Scandinavia, Central_Europe, Turkey,Baltics ,Eastern_Europe ,Iceland ,Romania ,Finland ,Germany ,France ,Switzerland ,Italy ,Poland ,Belgium ,Netherlands ,Luxembourg ,Morocco ,Algeria , Tunisia , Egypt ]
+All_groups = [British_Isles, Asian, Iberian_Peninsula, Southeast, Scandinavia, Central_Europe, Turkey,Baltics ,Eastern_Europe ,Iceland ,Germany ,France ,Switzerland ,Italy ,Poland ,Belgium ,Netherlands ,Luxembourg ,Morocco ,Algeria , Tunisia , Egypt ]
 
 summer = True   # used to select between pollution data for January and July
 
@@ -55,7 +55,6 @@ method = ct.METHOD_AVG  # the way that the data is combined inside one country (
 
 print("Creating country polygons...")
 countries = ct.create_country_polygons()
-areas = np.array(list(countries.values()))[:, 1]
 
 print("Retrieving raw pollution and emission data...")
 raw_data, _ = ct.find_poll_em_data(countries, poll_coll, em_chemical, poll_chemical,
@@ -65,35 +64,43 @@ print("Processing the data...")
 em_data, _ = ct.process_data(countries, raw_data, method=method, mode=ct.PLOT_EMISSIONS, multiplier=em_mult)
 poll_data, _ = ct.process_data(countries, raw_data, method=method, mode=ct.PLOT_POLLUTION, multiplier=poll_mult)
 
-d = []
+ed = []
 for group in All_groups:
     total = []
-    countries = group[1::]
+    c_in_group = group[1::]
     t = []
-    for country in countries:
-        total.append(em_data[country])
-    t.append(group.pop(0))
-    t.append(sum(total))
+    area = []
+    for country in c_in_group:
+        c_area = countries[country][1]
+        total.append(em_data[country]*c_area)
+        area.append(c_area)
+    t.append(group[0])
+    final = (sum(total)/(sum(area)))
+    t.append(final)
     t = tuple(t)
-    d.append(t)
-d = OrderedDict(d)
+    ed.append(t)
+ed = OrderedDict(ed)
 
-em_data = d
+em_data = ed
 
-d = []
+pd = []
 for group in All_groups:
     total = []
-    countries = group[1::]
+    c_in_group = group[1::]
     t = []
-    for country in countries:
-        total.append(poll_data[country])
-    t.append(group.pop(0))
-    t.append(sum(total))
+    area = []
+    for country in c_in_group:
+        c_area = countries[country][1]
+        total.append(poll_data[country] * c_area)
+        area.append(c_area)
+    t.append(group[0])
+    final = (sum(total) / (sum(area)))
+    t.append(final)
     t = tuple(t)
-    d.append(t)
-d = OrderedDict(d)
+    pd.append(t)
+pd = OrderedDict(pd)
 
-poll_data = d
+poll_data = pd
 
 print("Plotting...")
 em_values = np.array(list(em_data.values()))
@@ -103,36 +110,14 @@ plt.axis([min(em_values), max(em_values), min(poll_values), max(poll_values)])
 
 plt.scatter(em_values, poll_values, cmap='hsv', c=np.random.rand(len(em_values)))
 
-# for country in em_data:
-#     plt.annotate(country, [em_data[country], poll_data[country]])
+for country in em_data:
+    plt.annotate(country, [em_data[country], poll_data[country]])
 
 plt.title(ct.generate_sub_title(poll_chemical, em_chemical, summer, emission_levels, method))
 plt.xlabel(em_chemical + " Emission Mass from Aviation $[kg/day/km^2]$")
 plt.ylabel(("Average Ground-Level {} from Aviation " + "$[\mu g/m/km^2]$"  # not quite sure about the pollution units
             if poll_chemical != "SpeciesConc_O3" else "$[mol/(mol of dry air)/km^2]$").format(poll_chemical))
-plt.yscale('linear') #With this line you can change the type of graph
+plt.yscale('log') #With this line you can change the type of graph
+plt.xscale('log') # Double Logaritmic is the clearest
 print("Finished")
 plt.show()
-
-
-# em_data = dict([('Albania', 1.2887232830806852e-19), ('Algeria', 9.615877358485282e-21)])
-# poll_data = dict([('Albania', 3.962841252382609e-08), ('Algeria', 2.2985724546747333e-08)])
-
-# d = []
-# for group in All_groups:
-#     total = []
-#     countries = group[1::]
-#     t = []
-#     for country in countries:
-#         total.append(em_data[country])
-#     t.append(group.pop(0))
-#     t.append(sum(total))
-#     t = tuple(t)
-#     d.append(t)
-# d = OrderedDict(d)
-#
-# em_data = d
-#
-# print(d)
-# # d[group.pop(0)] = sum(total)
-# print(em_data)
