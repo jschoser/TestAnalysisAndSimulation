3fb1ae1d70955f7a7d2cc4127b609082e9e8a385
seba258 <olvc.sebastien.stevens@gmail.com>
2020-04-30 17:54:49 +0200

    i want to commit

--- Master program/Wind_rose.py
+++ Master program/Wind_rose.py
@@ -13,21 +13,21 @@ arr3 = np.array([])
 u=  np.array([])
 v=  np.array([])
 
-y=52.5
-x=4.375
+y=41
+x=-4.375
 
 #y 30.0 30.5 31.0 31.5 32.0 ... 68.0 68.5 69.0 69.5 70.0
 #x -30.0 -29.38 -28.75 -28.12 ... 48.12 48.75 49.38 50.0
 
 #wind directions ____________________________________________________________________________________________________
 
-dataDIR2 = 'E:/pythion/pythion wind/07/MERRA2.20050120.A3dyn.05x0625.EU.nc4'
+dataDIR2 = 'E:/pythion/pythion wind/07/MERRA2.20050720.A3dyn.05x0625.EU.nc4'
 DS2 = xr.open_dataset(dataDIR2)
 
-dataDIR3 = 'E:/pythion/pythion wind/07/MERRA2.20050121.A3dyn.05x0625.EU.nc4'
+dataDIR3 = 'E:/pythion/pythion wind/07/MERRA2.20050721.A3dyn.05x0625.EU.nc4'
 DS3 = xr.open_dataset(dataDIR3)
 
-dataDIR4 = 'E:/pythion/pythion wind/07/MERRA2.20050122.A3dyn.05x0625.EU.nc4'
+dataDIR4 = 'E:/pythion/pythion wind/07/MERRA2.20050722.A3dyn.05x0625.EU.nc4'
 DS4 = xr.open_dataset(dataDIR4)
 
 U = DS2.U.sel(lat=y, lon=x, lev=1, method='nearest')
@@ -61,14 +61,14 @@ print('this is not winddir anymore')
 
 #concentrations_________________________________________________________________________________________-
 
-dataDIR = '../data/PM25.1h.JAN.ON.nc4'
+dataDIR = '../data/PM25.1h.JUL.ON.nc4'
 DS = xr.open_dataset(dataDIR)
 
 da=DS.PM25.sel(lat=y, lon=x, method='nearest')
 
 print(DS.var)
 
-da = da.sel(time=slice('2005-01-20T00:30:00.000000000', '2005-01-22T23:30:00.000000000'))
+da = da.sel(time=slice('2005-07-20T00:30:00.000000000', '2005-07-22T23:30:00.000000000'))
 for i in range(24):
     a=(da[3*i]+da[3*i+1]+da[3*i+2])/3
     arr2=np.array([a])
@@ -77,14 +77,14 @@ print(arr1)
 
 #_______________________________________________________________________
 
-dataDIR = '../data/PM25.1h.JAN.OFF.nc4'
+dataDIR = '../data/PM25.1h.JUL.OFF.nc4'
 DS = xr.open_dataset(dataDIR)
 
 da=DS.PM25.sel(lat=y, lon=x, method='nearest')
 
 print(DS.var)
 
-da = da.sel(time=slice('2005-01-20T00:30:00.000000000', '2005-01-22T23:30:00.000000000'))
+da = da.sel(time=slice('2005-07-20T00:30:00.000000000', '2005-07-22T23:30:00.000000000'))
 
 for i in range(24):
     a=(da[3*i]+da[3*i+1]+da[3*i+2])/3
