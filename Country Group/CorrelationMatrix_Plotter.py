4babe0bae7309b9972dde28be6f8309eaaccde6c
jakob <jakob.schoser@aerodelft.nl>
2020-05-06 12:09:48 +0200

    Added scatter plots to correlation matrix

--- Country Group/CorrelationMatrix_Plotter.py
+++ Country Group/CorrelationMatrix_Plotter.py
@@ -4,8 +4,9 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sb
 
-summer = True
+summer = False
 emission_levels = slice(0, 32)
+with_scatterplots = False
 
 names = ["Fuelburn", "NO2", "HC", "CO", "BC", "PM25", "AerMassSO4", "AerMassNIT", "AerMassNH4", "AerMassPOA",
          "AerMassBC", "O3"]
@@ -19,7 +20,25 @@ df_lt = corr_df.where(np.tril(np.ones(corr_df.shape)).astype(np.bool))
 
 hmap = sb.heatmap(df_lt, cmap="coolwarm", vmin=-0.9, vmax=1, annot=True, cbar=False)
 
-plt.title("Correlation Matrix\n\nEmission levels: " + str(emission_levels.start) + " to " + str(emission_levels.stop) +
+plt.title("Emission levels: " + str(emission_levels.start) + " to " + str(emission_levels.stop) +
           " | " + ("July" if summer else "January") + " 2005")
 
-plt.show()
+if with_scatterplots:
+    plt.savefig("MatrixOutput/Correlation matrix")
+
+    for i in range(int(np.ceil(len(names) / 2))):
+        for j in range(len(names)):
+            if i != j:
+                plt.figure()
+                plt.title("Emission levels: " + str(emission_levels.start) + " to " + str(emission_levels.stop) +
+                      " | " + ("July" if summer else "January") + " 2005")
+                x = df[names[i]]
+                y = df[names[j]]
+                plt.scatter(x, y)
+                plt.axis([min(x), max(x), min(y), max(y)])
+                plt.xlabel(names[i])
+                plt.ylabel(names[j])
+                plt.savefig("MatrixOutput/Scatter plot " + names[i] + " vs " + names[j])
+
+else:
+    plt.show()
