700255833dc5e9aa53b07b19983e5296f6b518a4
egonbeyne <egon.beyne@gmail.com>
2020-05-09 17:20:53 +0200

    Min and max values of the colorbar don't include outliers anymore

--- High-Res Maps/High_resolution_avg.py
+++ High-Res Maps/High_resolution_avg.py
@@ -10,7 +10,7 @@ season = 'Summer'
 # Choose the pollutant
 pollutant = 'Ozone'
 
-# ====================================== Loading the datafile and -set =================================================
+# ============================================ Loading the datafile ====================================================
 
 # Datafile to be loaded
 Name_DF = (pollutant == 'BC') * 'Soot' + (pollutant == 'Ozone') * 'O3'
@@ -34,7 +34,7 @@ DF_av_off   = xr.open_dataset(filepath_off)
 DF_av_only = DF_av_on - DF_av_off
 
 
-# ==================================== Selecting and averaging pollutants ==============================================
+# ====================================== Selecting and averaging pollutants ============================================
 
 # Selecting the dataset containing the desired pollutant
 DS = getattr(DF_av_only, Name_DS)
@@ -45,6 +45,20 @@ DS_avg = DS.mean(dim = 'time')
 # Select pollutant data at ground level
 DS_avg_grd = DS_avg.sel(lev = 1, method = 'nearest')
 
+# ============================================ Detecting outliers ======================================================
+
+# Calculate the first and third quartile
+Q1      = DS_avg_grd.quantile(.25)
+Q3      = DS_avg_grd.quantile(.75)
+
+# Calculate the interquartile range
+IQR     = Q3 - Q1
+
+# Calculate limits for outliers
+# the bounds for vmax are greater than the standard 1.5 when BC is selected,
+# but otherwise all of central Europe was seen as an outlier
+vmin    = Q1 - (1.5*IQR)
+vmax    = Q3 + ((1.5 + 1.5*(pollutant == 'BC'))*IQR)
 
 # ============================================= Plotting Results =======================================================
 
@@ -58,7 +72,10 @@ ax = plt.axes(projection=proj)
 ax.coastlines(resolution='50m')
 
 # Plot the result
-im = DS_avg_grd.plot.pcolormesh(ax=ax, cmap='coolwarm', add_colorbar = False)
+im = DS_avg_grd.plot.pcolormesh(ax=ax, cmap='coolwarm',
+                                vmax = vmax,
+                                vmin = vmin,
+                                add_colorbar = False)
 
 # Make a colorbar
 cb = plt.colorbar(im, orientation="vertical", shrink = 0.55)
