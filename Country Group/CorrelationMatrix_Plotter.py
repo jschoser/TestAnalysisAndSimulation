00bf373890f3747ff50d7acf2bf2799016616a6d
jakob <jakob.schoser@aerodelft.nl>
2020-05-20 23:15:34 +0200

    Fixed mistake with matrix labelling

--- Country Group/CorrelationMatrix_Plotter.py
+++ Country Group/CorrelationMatrix_Plotter.py
@@ -6,21 +6,27 @@ import seaborn as sb
 
 summer = False
 emission_levels = slice(0, 32)
-save_with_scatterplots = False
+save_with_scatterplots = True
 with_labels = True
 
-names = ["Fuelburn", "NO$_{x}$", "HC", "CO", "BC", "PM$_{2.5}$", "SO$_{4}$", "NIT", "NH$_{4}$", "POA",
-         "BC", "O$_{3}$"]
+names = ["Fuelburn", "NO$_{x}$", "HC", "CO", "BC ", "O$_{3}$", "PM$_{2.5}$", "SO$_{4}$", "NIT", "NH$_{4}$", "POA",
+         "BC"]
 
 
 data = ct.find_matrix_data(ct.POLLUTION_COLLECTIONS, emission_levels, summer)
 df = pd.DataFrame.from_dict(data, orient='index', columns=names)
 
 corr_type = 'pearson'  # for a different correlation indicator, try method='spearman' or method='kendall'
-corr_df = corr = df.corr(method=corr_type)  # calculate correlation matrix
+corr_df = df.corr(method=corr_type)  # calculate correlation matrix
 df_lt = corr_df.where(np.tril(np.ones(corr_df.shape)).astype(np.bool))
 
-hmap = sb.heatmap(df_lt, cmap="coolwarm", annot=True, cbar=False)
+df_lt = df_lt.drop(names[:5])
+df_lt = df_lt.drop(names[5:], axis=1)
+
+plt.rcParams.update({'font.size': 12})
+hmap = sb.heatmap(df_lt, cmap="coolwarm", annot=True, cbar=True, vmin=-1, vmax=1)
+plt.xlabel("Emission species")
+plt.ylabel("Pollutant species")
 
 plt.title("Emission levels: " + str(emission_levels.start) + " to " + str(emission_levels.stop) +
           " | " + ("July" if summer else "January") + " 2005")
@@ -43,7 +49,7 @@ if save_with_scatterplots:
                 if with_labels:
                     for ci in range(len(x)):
                         plt.annotate(df.index.values[ci], [x[ci], y[ci]])
-                plt.savefig("MatrixOutput/Scatter plot " + names[i] + " vs " + names[j])
+                plt.savefig("MatrixOutput/Scatter plot " + (names[i] + " vs " + names[j]).replace(".", ""))
 
     print("All figures saved to current directory")
 
--- Country Group/map_plot.py
+++ Country Group/map_plot.py
@@ -33,12 +33,12 @@ shown in a map
 
 summer = False   # used to select between pollution data for January and July
 
-poll_coll = "SOOT.24h"  # the collection name for pollution (first part of the .nc4 filename)
+poll_coll = "O3.24h"  # the collection name for pollution (first part of the .nc4 filename)
 
 # the chemicals to be taken into account for pollution and emissions, respectively. These need to be the names of the
 # data sets inside the .nc4 files you selected
-poll_chemical = "AerMassBC"
-em_chemical = "BC"
+poll_chemical = "SpeciesConc_O3"
+em_chemical = "NO2"
 
 # the altitude levels over which emissions will be considered (available from 0 to 32). Check Altitude_levels.txt for
 # conversion to km. Level 8: 1 km altitude, level 32: 13 km altitude
@@ -48,7 +48,7 @@ emission_levels = slice(0, 32)
 # they make it impossible to see any differences between the other countries
 outliers = []  # ["Iraq", "Israel", "Latvia"]
 
-mode = ct.RETURN_RATIO  # the statistic which is plotted (emissions, pollution or ratio between them)
+mode = ct.RETURN_EMISSIONS  # the statistic which is plotted (emissions, pollution or ratio between them)
 method = ct.METHOD_AVG  # the way that the data is combined inside one country (median or area-weighted average)
 
 do_spatial_analysis = False  # whether a second figure with spatial autocorrelation indicators should be displayed
